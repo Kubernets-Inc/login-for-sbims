6585ad7f22e061a15910b2ff2c8ca314b59359c2
mjacques007 <43174669+mjacques007@users.noreply.github.com>
2018-09-21 20:25:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<?php include("header.php"); ?>	
+	<body>
+		<div id="block-login"><!-- on peut aussi utiliser le span-->
+
+
+			  <div class="user-image-login"><img src="images/user.png"></div>
+			<form method="post" action="#" id="connection">
+               	        	<p>
+               	        	  
+               	           	     <input type="text" name="username"/ id="username" placeholder="Username"></br></br>
+               	        	     <input type="password" name="password" id="username" placeholder="Password"/></br></br>
+               	        	     <input type="submit" value="Login" id="submit"/></br></br>
+               	        	      <a href="#" id="h5"><h5>Forgoten password?</h5></a>
+
+
+               	        	</p>
+               	        </form>
+
+
+	</div>
+	</body>
